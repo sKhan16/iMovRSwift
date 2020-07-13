dec8806f3bb055ff77ba213b0dbcf6ead96ae638
sKhan16 <khanboy411@hotmail.com>
2020-07-13 12:45:40 -0700

    First commit.

---
+++ iMovR/Pods/Target Support Files/Pods-iMovR-iMovRUITests/Pods-iMovR-iMovRUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iMovR_iMovRUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iMovR_iMovRUITests
+@end
---
+++ iMovR/iMovR/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  iMovR
+//
+//  Created by Shakeel Khan on 7/13/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("Home View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                }
+                .tag(0)
+            Text("Settings View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "gear")
+                        Text("Settings")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
