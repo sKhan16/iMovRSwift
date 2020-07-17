e0f715227839b9e8504448e12507b0c602e5dd2a
sKhan16 <khanboy411@hotmail.com>
2020-07-17 13:30:10 -0700

    mega f

---
+++ iMovR/iMovR/MotherView.swift
@@ -0,0 +1,21 @@
+//
+//  MotherView.swift
+//  iMovR
+//
+//  Created by Shakeel Khan on 7/17/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+import SwiftUI
+
+struct MotherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MotherView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotherView()
+    }
+}
