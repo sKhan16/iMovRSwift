c35e00eef9fc5694376f9206987a4b1c11ce6a92
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2020-07-18 14:47:01 -0700

    Update MotherView.swift

--- iMovR/iMovR/Views/MotherView.swift
+++ iMovR/iMovR/Views/MotherView.swift
@@ -9,17 +9,34 @@
 import SwiftUI
 
 struct MotherView: View {
+    // Variables here
+    
+    
     var body: some View {
-
-        Text("Mother View")
-        //        HStack {
-//            if viewState == 0 {
-//                onboardingView()
-//            }
-//            Text("Mother View")
-//        }
+        HStack {
+            Text("Mother View")
+            //self.pickView()
+            
+        }
     }
 }
+
+enum viewState: Int {
+    case onboarding = 0
+    case home = 1
+    case edit_presets = 2
+}
+ 
+//func pickView() -> AnyView {
+//    switch viewState(superViewState) {
+//        case onboarding:
+//            return onboardingView()
+//        case home:
+//            return HomeView()
+//        case edit_presets:
+//            return Text("error")
+//    }
+//}
 
 struct MotherView_Previews: PreviewProvider {
     static var previews: some View {
