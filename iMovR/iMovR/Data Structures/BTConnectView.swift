a0a647c28c908b20f74025dd774de3b1cb2c7caa
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2020-07-17 18:56:20 -0700

    bt and mother view updates

---
+++ iMovR/iMovR/Data Structures/BTConnectView.swift
@@ -0,0 +1,23 @@
+//
+//  BTConnectView.swift
+//  iMovR
+//
+//  Created by Michael Humphrey on 7/17/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+import SwiftUI
+
+struct BTConnectView: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct BTConnectView_Previews: PreviewProvider {
+    static var previews: some View {
+        BTConnectView()
+    }
+}
--- iMovR/iMovR/MotherView.swift
+++ iMovR/iMovR/MotherView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct MotherView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if viewState == 0 {
+                onboardingView()
+            }
+            Text("Mother View")
+        }
     }
 }
 
