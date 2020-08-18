74b99da18a42593942a6123c83b3f9b1641fe629
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2020-08-17 17:10:07 -0700

    desk setting detail

---
+++ iMovR/iMovR/Settings/DeskSettingDetail.swift
@@ -0,0 +1,110 @@
+//
+//  DeskSettingDetail.swift
+//  iMovR
+//
+//  Created by Michael Humphrey on 8/17/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+import SwiftUI
+
+struct DeskSettingDetail: View {
+
+    //TODO: Get needed variables
+    // var name: String
+    //@EnvironmentObject var user: UserObservable
+    //TODO: MAKE FUNCTION TO EDIT NAME
+    //MAKE FUNCTION TO DELETE PRESET
+    //var preset: self.user.presets
+    var currIndex: Int
+    //var name: String
+    @EnvironmentObject var user: UserObservable
+    
+    @State var canDelete: Bool = false
+    @State var showAlert: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            
+            NavigationLink(destination: EditPreset(currIndex: self.currIndex)) {
+                /*Button(action: {print("TEST")}) {
+                 Text("Edit Name")
+                 .padding()
+                 .background(Color.gray)
+                 .foregroundColor(.white)
+                 .font(.title)*/
+                Text("EDIT PRESET")
+                //}
+                
+                //.padding()
+            }.buttonStyle(PlainButtonStyle())
+                
+                .padding()
+            
+            Button (action: {
+                self.showAlert = true
+                //if (self.canDelete) {
+                //self.user.presets.remove(at: self.currIndex)
+                //print("Removed")
+                //self.canDelete = false
+                //}
+                
+            }) {
+                Text("DELETE PRESET")
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Delete preset?"), primaryButton: .default(Text("Confirm")) {
+                    if !self.canDelete {
+                        self.canDelete = true
+                    }
+                    if self.canDelete {
+                        print("Removed")
+                        
+                        ///Crashes the app, can't delete from index
+                        //self.user.presets.remove(at: self.currIndex)
+                        
+                        self.canDelete = false
+                    }
+                    },  secondaryButton: .destructive(Text("Cancel")))
+            }
+            /*
+             
+             Button(action: {print("TEST")}) {
+             Text("Delete")
+             .padding()
+             .background(Color.gray)
+             .foregroundColor(.white)
+             .font(.title)
+             }*/
+        }
+        //.navigationBarTitle(Text(""))
+        //.navigationBarHidden(true)
+    }
+}
+
+
+struct confirmAlert: View {
+    
+    @EnvironmentObject var user: UserObservable
+    
+    @Binding var showAlert: Bool
+    @Binding var canDelete: Bool
+    //var currIndex: Int
+    
+    var body: some View {
+        Button(action: {
+            self.canDelete = true
+        }) {
+            Text("Confirm")
+        }
+    }
+}
+
+
+
+struct DeskSettingDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        DeskSettingDetail(currIndex: 0)
+    }
+}
