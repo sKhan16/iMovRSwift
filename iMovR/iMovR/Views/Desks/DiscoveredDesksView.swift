d3bff9b75e5f1418ff3ffd365c3604a34e2a4f9f
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2020-09-18 19:52:59 -0700

    Update DiscoveredDesksView.swift

--- iMovR/iMovR/Views/Desks/DiscoveredDesksView.swift
+++ iMovR/iMovR/Views/Desks/DiscoveredDesksView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct DiscoveredDesksView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            NavigationLink(destination: BTConnectView)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle("Connect To Desk")
+        }
     }
 }
 
