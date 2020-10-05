89ef67c2f904fa017ad870a05048d88172807c2e
sKhan16 <khanboy411@hotmail.com>
2020-10-05 12:39:14 -0700

    Initial commit to branch

---
+++ iMovR/iMovR/Views/Main Screen/HomeViewV2.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewV2.swift
+//  iMovR
+//
+//  Created by Shakeel Khan on 10/5/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeViewV2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewV2_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewV2()
+    }
+}
