ce9d0811639c58c4b689c41772d97dacdc5509a9
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2020-10-07 11:05:07 -0700

    Device SuperClass for all Bluetooth devices

---
+++ iMovR/iMovR/Data Structures/Device.swift
@@ -0,0 +1,25 @@
+//
+//  Device.swift
+//  iMovR
+//
+//  Created by Michael Humphrey on 10/7/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+/**
+#Device.swift
+    is the superclass of all supported iMovR app Bluetooth devices
+    **Supported Devices:**
+    - ZGo ZipDesk by iMovR
+ 
+    *Support in future for: *
+    - *Linak Lander Desks*
+    - *Linak Monitor Arms*
+    - *Desk Treadmills*
+*/
+
+import Foundation
+
+class Device {
+    
+}
