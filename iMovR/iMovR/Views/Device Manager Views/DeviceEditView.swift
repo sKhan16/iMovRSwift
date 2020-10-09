68d487f9cfc0e1161b095aad244cfdb47c44ad21
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2020-10-08 18:16:43 -0700

    Device Edit beginnings

---
+++ iMovR/iMovR/Views/Device Manager Views/DeviceEditView.swift
@@ -0,0 +1,21 @@
+//
+//  DeviceEditView.swift
+//  iMovR
+//
+//  Created by Michael Humphrey on 10/8/20.
+//  Copyright © 2020 iMovR. All rights reserved.
+//
+
+import SwiftUI
+
+struct DeviceEditView: View {
+    var body: some View {
+        Text("device edit view")
+    }
+}
+
+struct DeviceEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeviceEditView()
+    }
+}
--- iMovR/iMovR/Views/Device Manager Views/DeviceManagerView.swift
+++ iMovR/iMovR/Views/Device Manager Views/DeviceManagerView.swift
@@ -10,87 +10,81 @@ import SwiftUI
 
 struct DeviceManagerView: View {
     
-    //use z stack popup stuff to get edit device menu(s)
+    @State var editDeviceIndex: Int = -1
     
     var body: some View {
-        VStack {
-            Text("Device Manager")
-                .font(Font.largeTitle.bold())
-                .foregroundColor(Color.white)
-                .padding()
-            ScrollView {
-                Text("Saved Devices")
+        ZStack {
+            VStack {
+                Text("Device Manager")
+                    .font(Font.largeTitle.bold())
                     .foregroundColor(Color.white)
-                    .font(Font.title2)
-                    .padding(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                ForEach(Range(0...6)) { index in
-                    VStack {
-                        DeviceRowView(deviceIndex: index)
-                        //.cornerRadius(10.0)
-                        .padding(2)
-                    }
-                }
-                    //.border(Color.blue, width: 3)
-                    .background(ColorManager.bgColor)
-                    .cornerRadius(20.0)
-                    //.padding(5)
-                    .frame(maxWidth: .infinity)
-                
-                Text("DiscoveredDevices")
-                    .foregroundColor(Color.white)
-                    .font(Font.title2)
-                    .padding([.leading,.top])
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                ForEach(Range(6...8)) { index in
-                    VStack {
-                        DeviceRowView(deviceIndex: index)
-                            .padding(2)
-                    }
+                    .padding()
+                ScrollView {
+                    Text("Saved Devices")
+                        .foregroundColor(Color.white)
+                        .font(Font.title2)
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     
+                    ForEach(Range(0...6)) { index in
+                        VStack {
+                            DeviceRowView(deviceIndex: index)
+                            //.cornerRadius(10.0)
+                            .padding(2)
+                        }
+                    }
+                        //.border(Color.blue, width: 3)
+                        .background(ColorManager.bgColor)
+                        .cornerRadius(20.0)
+                        //.padding(5)
+                        .frame(maxWidth: .infinity)
+                    
+                    Text("DiscoveredDevices")
+                        .foregroundColor(Color.white)
+                        .font(Font.title2)
+                        .padding([.leading,.top])
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    ForEach(Range(6...8)) { index in
+                        VStack {
+                            DeviceRowView(deviceIndex: index)
+                                .padding(2)
+                        }
+                        
+                    }
+                    //.border(Color.yellow, width: 3)
+                    .background(ColorManager.bgColor)
+                    .cornerRadius(15.0)
+                    .frame(maxWidth: .infinity)
                 }
-                //.border(Color.yellow, width: 3)
-                .background(ColorManager.bgColor)
-                .cornerRadius(15.0)
-                .frame(maxWidth: .infinity)
-            }
-            .padding(2)
-            //.border(Color.red, width: 3)
+                .padding(2)
+                //.border(Color.red, width: 3)
+                
+            }//end VStack
             
-        }//end VStack
-    }
+            // Popup for editing saved device properties
+            if (editDeviceIndex != -1) {
+                DeviceEditView(deviceIndex: $editDeviceIndex)
+            }
+        }.animation(.easeInOut)//end ZStack
+    } //end Body
+    
 }
 
 struct DeviceManagerView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            Group {
-                ZStack {
-                    ColorManager.bgColor.edgesIgnoringSafeArea(.all)
-                    
-                    DeviceManagerView()
-                }
-                ZStack {
-                    ColorManager.bgColor.edgesIgnoringSafeArea(.all)
-                    
-                    DeviceManagerView()
-                }
-                .previewDevice("iPhone 6s")
+            ZStack {
+                ColorManager.bgColor.edgesIgnoringSafeArea(.all)
+                
+                DeviceManagerView()
             }
-            Group {
-                ZStack {
-                    ColorManager.bgColor.edgesIgnoringSafeArea(.all)
-                    
-                    DeviceManagerView()
-                }
-                ZStack {
-                    ColorManager.bgColor.edgesIgnoringSafeArea(.all)
-                    
-                    DeviceManagerView()
-                }
+            ZStack {
+                ColorManager.bgColor.edgesIgnoringSafeArea(.all)
+                
+                DeviceManagerView()
             }
+            .previewDevice("iPhone 6s")
         }
     }
 }
