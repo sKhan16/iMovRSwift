2be67d675e30e541626727b74447111e4043c4aa
MichaelHumphrey7 <37855531+MichaelHumphrey7@users.noreply.github.com>
2021-03-04 18:39:37 -0800

    Removed oversized shadow buttons, added blue connect icon, more Device Manager gfx changes

--- iMovR/iMovR/Views/Device Manager Views/SavedDeviceRowView.swift
+++ iMovR/iMovR/Views/Device Manager Views/SavedDeviceRowView.swift
@@ -43,9 +43,9 @@ struct SavedDeviceRowView: View {
                             .font(Font.title3.bold())
                             .truncationMode(.tail)
                             .foregroundColor(ColorManager.royalBlue)
-                        Text("("+String(currDevice.id)+")")
-                            .font(Font.caption)//.weight(.medium))
-                            .foregroundColor(ColorManager.royalBlue)
+//                        Text("("+String(currDevice.id)+")")
+//                            .font(Font.caption)//.weight(.medium))
+//                            .foregroundColor(ColorManager.royalBlue)
                     }.offset(y: -3)
                     if !bt.bluetoothEnabled {
                         Text("Phone Bluetooth Disabled")
@@ -133,49 +133,69 @@ private struct ConnectButton: View {
             { // Button View 'label'
                 if self.bt.bluetoothEnabled,
                    self.bt.data.savedDevices.indices.contains(deviceIndex),
-                   self.bt.data.savedDevices[deviceIndex].peripheral != nil {
-                    
+                   self.bt.data.savedDevices[deviceIndex].peripheral != nil
+                {
                     ZStack {
-                        Circle()
-                            .foregroundColor(isConnected ? ColorManager.connectYellow : ColorManager.bgColor)
-                            .aspectRatio(contentMode: .fit)
-                            .shadow(color: .black, radius: 2)
-                        if isConnected {
-                            Image(systemName: "iphone.homebutton.slash")
-                                .resizable()
-                                .accentColor(ColorManager.deviceBG)
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 40)
-                            Image(systemName: "xmark.circle.fill")
-                                .resizable()
-                                .accentColor(Color.red)
-                                .aspectRatio(contentMode: .fit)
-                                .background(ColorManager.connectYellow.cornerRadius(20).frame(width: 18, height: 18))
-                                .frame(height: 20)
-                                .offset(x: 25, y: -20)
-                                .shadow(color: ColorManager.connectYellow, radius: 1)
-                        } else {
-                            Image(systemName: "iphone.homebutton.radiowaves.left.and.right") //"dot.radiowaves.left.and.right")
-                                .resizable()
-                                .accentColor(ColorManager.morePreset)
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 40)
-                        }
-                        
-                    }
-                } else {
-                    ZStack {
-                        Circle()
-                            .foregroundColor(Color.gray)
-                            .aspectRatio(contentMode: .fit)
-                            .shadow(color: ColorManager.gray, radius: 2)
-                        Image(systemName: "iphone.homebutton.radiowaves.left.and.right") //"dot.radiowaves.left.and.right")
+                        Image("ConnectBlue")
                             .resizable()
-                            .accentColor(ColorManager.gray)
                             .aspectRatio(contentMode: .fit)
                             .frame(height: 40)
-                            .opacity(0.6)
+                        Image("shdwButtonUnpressed")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
                     }
+//                    ZStack {
+//                        Circle()
+//                            .foregroundColor(isConnected ? ColorManager.connectYellow : ColorManager.bgColor)
+//                            .aspectRatio(contentMode: .fit)
+//                            .shadow(color: .black, radius: 2)
+//                        if isConnected {
+//                            Image(systemName: "iphone.homebutton.slash")
+//                                .resizable()
+//                                .accentColor(ColorManager.deviceBG)
+//                                .aspectRatio(contentMode: .fit)
+//                                .frame(height: 40)
+//                            Image(systemName: "xmark.circle.fill")
+//                                .resizable()
+//                                .accentColor(Color.red)
+//                                .aspectRatio(contentMode: .fit)
+//                                .background(ColorManager.connectYellow.cornerRadius(20).frame(width: 18, height: 18))
+//                                .frame(height: 20)
+//                                .offset(x: 25, y: -20)
+//                                .shadow(color: ColorManager.connectYellow, radius: 1)
+//                        } else {
+//                            Image(systemName: "iphone.homebutton.radiowaves.left.and.right") //"dot.radiowaves.left.and.right")
+//                                .resizable()
+//                                .accentColor(ColorManager.morePreset)
+//                                .aspectRatio(contentMode: .fit)
+//                                .frame(height: 40)
+//                        }
+//
+//                    }
+                } else {
+                    ZStack {
+                        Image("ConnectBlue")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                        Image("shdwButtonPressed")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                    }
+//                    ZStack {
+//                        Circle()
+//                            .foregroundColor(Color.gray)
+//                            .aspectRatio(contentMode: .fit)
+//                            .shadow(color: ColorManager.gray, radius: 2)
+//                        Image(systemName: "iphone.homebutton.radiowaves.left.and.right") //"dot.radiowaves.left.and.right")
+//                            .resizable()
+//                            .accentColor(ColorManager.gray)
+//                            .aspectRatio(contentMode: .fit)
+//                            .frame(height: 40)
+//                            .opacity(0.6)
+//                    }
                 }
             } //end button
         }
